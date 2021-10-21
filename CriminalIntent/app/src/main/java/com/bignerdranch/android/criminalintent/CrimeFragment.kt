83e775be676fc65c4c7470a8bbce567087956dda
kaluogh <zhangwenxin6615006@163.com>
2021-10-21 10:13:16 +0800

    add c8

---
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
@@ -0,0 +1,37 @@
+package com.bignerdranch.android.criminalintent
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CrimeFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = CrimeFragment()
+    }
+
+    private lateinit var crime: Crime
+    private lateinit var viewModel: CrimeViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        crime = Crime()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.crime_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(CrimeViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
